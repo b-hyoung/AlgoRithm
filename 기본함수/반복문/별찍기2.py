8b82fddbbbef0bd059145f969c6f5213f3aa0b4d
b-hyoung <youqlrqod@gmail.com>
2024-12-21 00:15:41 +0900

    별찍기 - 2.py

--- 기본함수/반복문/별찍기2.py
+++ 기본함수/반복문/별찍기2.py
@@ -1,6 +1,8 @@
-n = int(input(()))
+n = int(input())
 
 for i in range(n):
-    for j in range(n-i):
+    for j in range(n-(i+1)):
         print(" ",end="")
-    print("*")
+    for x in range(i+1):
+        print("*", end="")
+    print("")
