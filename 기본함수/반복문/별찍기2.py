8721d5c52d3433bdf15824a92740e184759f1d52
b-hyoung <youqlrqod@gmail.com>
2024-12-22 06:05:40 +0900

    pert 별찍기 - 2.py

--- 기본함수/반복문/별찍기2.py
+++ 기본함수/반복문/별찍기2.py
@@ -1,8 +1,16 @@
-n = int(input())
+# n = int(input())
 
+# for i in range(n):
+#     for j in range(n-(i+1)):
+#         print(" ",end="")
+#     for x in range(i+1):
+#         print("*", end="")
+#     print("")
+n = int(input())
+star = "*"
 for i in range(n):
-    for j in range(n-(i+1)):
-        print(" ",end="")
-    for x in range(i+1):
-        print("*", end="")
-    print("")
+    index = i+1
+    idx= n-index
+    if(index-n < 0):
+        print(" "*idx,end= "")
+    print("*"*index)
