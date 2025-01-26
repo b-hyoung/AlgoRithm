7eef256040dc3fc1d219c2247bb4768b7413dec8
b-hyoung <youqlrqod@gmail.com>
2025-01-26 23:38:50 +0900

    fix gugudan for -> while

--- 기본함수/반복문/구구단.py
+++ 기본함수/반복문/구구단.py
@@ -1,4 +1,13 @@
 num = int(input())
+t = 0
 
-for i in range(1,10):
-    print(num,"*",i,"=",num * i)
+while num :
+    t = t+1
+    if(t < 10):
+        print(num,"*",t,"=",num * t)
+    else:
+        break
+
+# for문    
+#     print(num,"*",i,"=",num * i)
+# for i in range(1,10):
